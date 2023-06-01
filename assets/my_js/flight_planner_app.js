633560f78405f96fdbac9855b1ecabfd33f2e24c
Ryan <ryan.elandt@gmail.com>
2023-06-01 12:25:32 -0400

    added path drawer and city names to app

--- assets/my_js/flight_planner_app.js
+++ assets/my_js/flight_planner_app.js
@@ -1,100 +1,48 @@
 import * as THREE from './three.module.min.js';
 
+import { CSS2DRenderer, CSS2DObject } from './CSS2DRenderer.js';
+
 import Module from './flight_planner.js';
 
-var ModObj;
 
+// The Three.js scene is set up using the city positions from the wasm module.
+// The wasm module is loaded asynchronously, so the Three.js scene must be initialized
+// after the wasm module is loaded. This is done by wrapping the initialization of the
+// Three.js scene in a promise that is resolved when the wasm module is loaded.
 Module().then((instance) => {
   ModObj = instance;
   console.log("Promise resolved!");
   init();
-  // render();
-  // animate();
 });
 
+let ModObj;
+let canvas_holder, canvas_helper, helper_cpp;
+let camera, scene, raycaster, renderer, labelRenderer;
+let markers;
+let camera_vectors;
 
-let canvas_holder, canvas_helper;
-let camera, scene, raycaster, renderer;
-
-let helper_cpp;
-let path_rad = 1.5;
-let v_cylinders = [];
-
-let rgb_puck_inner = 0xffddbb;  // Yellow 
-let rgb_puck_outer = 0x398464;  // Green
-let rgb_path       = 0x88722E;  // Brown
+// Colors
+const rgb_puck_inner = 0xffddbb;  // Yellow 
+const rgb_puck_outer = 0x398464;  // Green
+const rgb_path       = 0x88722E;  // Brown
 
 // Geometry Control Panel
-let puck_size = 3.5;
-let puck_height = 0.4;
-let puck_dist_lower = 500;
-var puck_dist_prop = 1.0001;
+const path_rad = 1.6;
+const puck_size = 3.5;
+const puck_height = 0.4;
+const EARTH_RAD = 500;
+const puck_dist_prop = 1.0001;
 
 const mouse_xy = new THREE.Vector2();
 
-// custom global variables
-var vStartEndCity = [];
-var vCity = [];
-var v_city_puck_inner = [];
-var v_city_puck_outer = [];
-var dictObIdToCityId = {};
-
+// Global variables that store stuff
+let v_cylinders = [];
+let vCity = [];
+let v_city_puck_inner = [];
+let v_city_puck_outer = [];
+let dictObIdToCityId = {};
 
 //////////////////////////////////////////
-
-
-// Class sizes and resizes the canvas and camera
-class CanvasHelper {
-  constructor() {
-    this.aspect_ratio_ = 1.7;
-    this.width_min_ = 400;
-    this.frustumSize_ = 215;
-  }
-
-  // Creates the camera
-  create_camera() {
-    const fSize = this.frustumSize();
-    return new THREE.OrthographicCamera(
-      fSize * this.aspect_ratio_ / - 2,
-      fSize * this.aspect_ratio_ / 2,
-      fSize / 2,
-      fSize / - 2, 1, 1000
-    );
-  }
-
-  // Sets the size of the canvas and renderer
-  set_canvas_holder_size(canvas_holder, renderer) {
-    const width = canvas_holder.clientWidth;
-    const height = width / this.aspect_ratio();
-    canvas_holder.style.height = height + "px";
-    
-    renderer.setSize(width, height);
-    this.print_canvas_holder_size(canvas_holder);
-  }
-  
-  // Prints the size of the canvas
-  print_canvas_holder_size(ch) { 
-    const width = ch.clientWidth;
-    const height = ch.clientHeight;
-    console.log("width: " + width + " height: " + height);
-  }
-  
-  aspect_ratio() { return this.aspect_ratio_; }
-  width_min() { return this.width_min_; }
-  frustumSize() { return this.frustumSize_; }
-}
-
-
-// Class stores the id and position of a city
-class City {
-  constructor(id, position) {
-    this.id = id;
-    this.position = position;  // Position is a THREE.Vector3
-  }
-
-  getId() { return this.id; }
-  getPosition() { return this.position; }
-}
 
 
 // Class Allocates a C++ array and frees it when it is destroyed
@@ -122,12 +70,9 @@ class CppArrayPointerHelper {
 
   // Loads the Cartesian positions of each city in the planner.
   loadCityData() {
-    var fun_name = 'voidAirportXyz';
-    puck_dist_lower = 500;
-    
     // Call the function in the wasm module
     ModObj.ccall( 
-      fun_name,
+      'voidAirportXyz',
       'void',
       ['number', 'number'], 
       [this.dataPtr, this.lengthPtr],
@@ -140,9 +85,9 @@ class CppArrayPointerHelper {
     var resultArray = [];
     for (var i = 0; i < this.getLength(); i += 3) {
       resultArray.push([
-        linear_city_xyz[i]     * puck_dist_lower,
-        linear_city_xyz[i + 1] * puck_dist_lower,
-        linear_city_xyz[i + 2] * puck_dist_lower
+        linear_city_xyz[i]     * EARTH_RAD,
+        linear_city_xyz[i + 1] * EARTH_RAD,
+        linear_city_xyz[i + 2] * EARTH_RAD
       ]);
     }
   
@@ -151,7 +96,6 @@ class CppArrayPointerHelper {
 
   // Calculates the sequence of cities that make up the minimum time path from id_src to id_dst.
   loadPathReal(id_src, id_dst) {
-
     // Call the path planning function in the wasm module
     ModObj.ccall(
       'voidPathOutReal',
@@ -176,7 +120,247 @@ class CppArrayPointerHelper {
   
     return resultArray;
   }
+
+  // Gets the name of the city
+  get_city_string_for_id(id) {
+    // Calls the city name function in the wasm module
+    ModObj.ccall(
+      'getCityName',
+      'void',
+      ['number', 'number', 'number'],
+      [this.dataPtr, this.lengthPtr, id],
+    );
+  
+    // Interprets the Cpp array as a JavaScript array
+    var linear_path = new Float64Array(ModObj.HEAPF64.buffer, this.dataPtr, this.n);
+  
+    // Create JavaScript string from linear_path
+    var string = "";
+    for (var i = 0; i < this.getLength(); ++i) {  
+      var ind_double = linear_path[i];
+      var ind = Math.round(ind_double); 
+      if (ind != ind_double) { throw new Error('ind is not an integer'); }
+
+      string += String.fromCharCode(ind);
+    }
+  
+    return string;
+  }
 }
+
+// Class sizes and resizes the canvas and camera
+class CanvasHelper {
+  constructor() {
+    this.aspect_ratio_ = 1.7;
+    this.width_min_ = 400;
+    this.frustumSize_ = 215;
+  }
+
+  // Creates the camera
+  create_camera() {
+    const fSize = this.frustumSize();
+    return new THREE.OrthographicCamera(
+      fSize * this.aspect_ratio_ / - 2,
+      fSize * this.aspect_ratio_ / 2,
+      fSize / 2,
+      fSize / - 2, 1, 1000
+    );
+  }
+
+  // Sets the size of the canvas and renderer
+  set_canvas_holder_size(canvas_holder, renderer, labelRenderer) {
+    const width = canvas_holder.clientWidth;
+    const height = width / this.aspect_ratio();
+    canvas_holder.style.height = height + "px";
+    
+    renderer.setSize(width, height);
+    labelRenderer.setSize(width, height);
+
+    this.print_canvas_holder_size(canvas_holder);
+  }
+  
+  // Prints the size of the canvas
+  print_canvas_holder_size(ch) { 
+    const width = ch.clientWidth;
+    const height = ch.clientHeight;
+    console.log("width: " + width + " height: " + height);
+  }
+  
+  aspect_ratio() { return this.aspect_ratio_; }
+  width_min() { return this.width_min_; }
+  frustumSize() { return this.frustumSize_; }
+}
+
+// Stores the camera rotation so the components can be reused
+class CameraVectors {
+  constructor() {
+    this.lookAt = new THREE.Vector3(0, 0, 0);
+
+    // x moves right
+
+    // this.uv_z = new THREE.Vector3(-168, -1000, 800).normalize();
+    this.uv_z = new THREE.Vector3(-165, -990, 800).normalize();
+
+    const uv_x_des = new THREE.Vector3(1.0, -0.32, 0).normalize();
+    this.uv_y = new THREE.Vector3().crossVectors(this.uv_z, uv_x_des).normalize();
+    this.uv_x = new THREE.Vector3().crossVectors(this.uv_y, this.uv_z).normalize();
+
+    // Turn uv_x, uv_y, uv_z into a rotation matrix
+    this.uv_mat = new THREE.Matrix4();
+    this.uv_mat.set(
+      this.uv_x.x, this.uv_y.x, this.uv_z.x, 0,
+      this.uv_x.y, this.uv_y.y, this.uv_z.y, 0,
+      this.uv_x.z, this.uv_y.z, this.uv_z.z, 0,
+      0, 0, 0, 1
+    );
+  }
+
+  getUvIntoViewer() { return this.uv_z.negate(); }
+}
+
+// Deals with the markers
+class Markers {
+  constructor() {
+    this.mark_path_last = createMarkerPath();
+    this.mark_path_snl = createMarkerPath();
+    this.mark_txt_last = createMarkerClear();
+    
+    // Add to scene
+    scene.add(this.mark_path_last);
+    scene.add(this.mark_path_snl);
+    scene.add(this.mark_txt_last);
+
+    this.vStartEndCity = [];
+    this.mark_txt_div = this.createTextDiv();
+  }
+
+  // Creates a text div element attached to the document
+  createTextDiv() {
+    const txtDiv = document.createElement( 'div' );
+    txtDiv.className = 'label';
+    txtDiv.innerHTML = '';
+    txtDiv.style.backgroundColor = 'transparent';
+    txtDiv.style.color = 'black';
+    txtDiv.style.fontSize = '18px';
+    return txtDiv;
+  }
+
+  setObjPosition(obj_set, x, y) {
+    const uv_x = camera_vectors.uv_x;
+    const uv_y = camera_vectors.uv_y;
+    const uv_z = camera_vectors.uv_z;
+
+    // Calculates the position of the object
+    var v_out = uv_z.clone();
+    v_out.add(uv_x.clone().multiplyScalar(x));
+    v_out.add(uv_y.clone().multiplyScalar(y));
+    v_out.normalize();
+    v_out.multiplyScalar(EARTH_RAD * puck_dist_prop);
+
+    // Sets the position of the object
+    obj_set.position.set(v_out.x, v_out.y, v_out.z);
+  }
+
+  // Records the last two cities that have been clicked
+  recordClickedCities(id) {
+    if (this.vStartEndCity.length <= 1) {
+      this.vStartEndCity.push(id);
+      return -1;
+    } else {
+      // remove the Start city from the list 
+      const id_remove = this.vStartEndCity.shift();
+      this.vStartEndCity.push(id);
+      return id_remove;
+    }
+  }
+
+  updateMarkers(id_click) {
+    this.recordClickedCities(id_click);
+
+    const id_last = this.getCityIdLast();
+    this.setObjPositionToCity(id_last, this.mark_path_last);
+    this.setObjPosition(this.mark_txt_last, -0.393, -0.2105);
+    this.setDivTextToCityName();
+
+    const id_snl = this.getCityIdSnl();
+    if (id_snl != null) {
+      this.setObjPositionToCity(id_snl, this.mark_path_snl);
+    }
+  }
+
+  getCityIdLast() {
+    if (this.vStartEndCity.length == 0) { return null; }
+    const id = this.vStartEndCity[this.vStartEndCity.length - 1];
+    return dictObIdToCityId[id];
+  }
+
+  // Gets the second to last city id
+  getCityIdSnl() {
+    if (this.vStartEndCity.length == 1) { return null; }
+    const id = this.vStartEndCity[this.vStartEndCity.length - 2];
+    return dictObIdToCityId[id];
+  }
+
+  setObjPositionToCity(id_city, obj) {
+    const xyz = vCity[id_city].getPosition();
+    var xyz_scale = xyz.clone().multiplyScalar(1.01);
+    obj.position.set(xyz_scale.x, xyz_scale.y, xyz_scale.z);
+  }
+
+  initText() { this.attachTextToObject(this.mark_txt_last, this.mark_txt_div); }
+
+  setDivTextToCityName() {
+    const id = this.getCityIdLast();
+    const string = helper_cpp.get_city_string_for_id(id);
+    this.mark_txt_div.innerHTML = string;
+  }
+
+  attachTextToObject(attach_to, txtDiv) {
+    attach_to.layers.enableAll();
+    
+    const txt_label = new CSS2DObject( txtDiv );
+    
+    // Sets label position 10 units to the right of the marker
+    const DDD = 9;
+    txt_label.position.set( DDD, 0, 0 );
+
+    txt_label.center.set( 0, 0.5 );
+    attach_to.add( txt_label );
+    txt_label.layers.set( 0 );
+  }
+}
+
+// Class stores the id and position of a city
+class City {
+  constructor(id, position) {
+    this.id = id;
+    this.position = position;  // Position is a THREE.Vector3
+  }
+
+  getId() { return this.id; }
+  getPosition() { return this.position; }
+}
+
+function create_circle_black() {
+  var obj_geometry = new THREE.CircleGeometry( puck_size * 1.0, 32 );
+  var obj_material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
+  return new THREE.Mesh(obj_geometry, obj_material);
+}
+
+function create_circle_transparent() {
+  var obj_geometry = new THREE.CircleGeometry( puck_size * 1.0, 32 );
+  var obj_material = new THREE.MeshBasicMaterial( { color: 0x000000, transparent: true, opacity: 0.0 } );
+  return new THREE.Mesh(obj_geometry, obj_material);
+}
+
+function createMarker(marker_obj) {
+  marker_obj.position.set(0, 0, -1000);
+  marker_obj.rotation.setFromRotationMatrix(camera_vectors.uv_mat);
+  return marker_obj;
+}
+
+function createMarkerClear() { return createMarker(create_circle_transparent()); }
+function createMarkerPath() { return createMarker(create_circle_black()); }
 
 function createCylGeoForHeight(r, h) { return new THREE.CylinderGeometry(r, r, h, 16, 1, false); }
 function createNewCylinder(r) {
@@ -216,25 +400,6 @@ function adjustCylinder(cylinder, xyz_0, xyz_1, is_reuse_height=false) {
   cylinder.quaternion.setFromUnitVectors(new THREE.Vector3(0, 1, 0), direction);
 }
 
-function adjustRaise(obj_in) {
-  const xyz = obj_in.position;
-  // Assert that xyz is not the zero vector
-  if (xyz.x == 0 && xyz.y == 0 && xyz.z == 0) { throw new Error('xyz is the zero vector'); }
-  xyz.normalize();
-  xyz.multiplyScalar(puck_dist_lower * puck_dist_prop);
-  obj_in.position.set(xyz.x, xyz.y, xyz.z);
-}
-
-function adjustLower(obj_in) {
-  const xyz = obj_in.position;
-  // Assert that xyz is not the zero vector
-  if (xyz.x == 0 && xyz.y == 0 && xyz.z == 0) { throw new Error('xyz is the zero vector'); }
-  xyz.normalize();
-  xyz.multiplyScalar(puck_dist_lower);
-  obj_in.position.set(xyz.x, xyz.y, xyz.z);
-}
-
-
 // Initializes the geometry of the scene
 function initGeometry() {
   const n_doubles_allocate = 303 * 3;
@@ -252,6 +417,8 @@ function initGeometry() {
   var outlineGeometry = createCylGeoForHeight(puck_size * 2.0, puck_height * 0.5);
   var outlineMaterial = new THREE.MeshBasicMaterial( { color: rgb_puck_outer, side: THREE.BackSide } );
   
+  markers = new Markers();
+
   // Iterate over vCity
   for (let i = 0; i < vCity.length; i++) {
     const object = new THREE.Mesh( puck_inner,
@@ -281,13 +448,15 @@ function initGeometry() {
     dictObIdToCityId[object.id] = i;
     dictObIdToCityId[outlineMesh.id] = i;
   }
-}
 
+  markers.initText();
+}
 
 // Initializes the scene
 function init() {
   // Gets the canvas-holder html element
   canvas_holder = document.getElementById("canvas-holder");
+  camera_vectors = new CameraVectors();
   
   canvas_helper = new CanvasHelper();
   camera = canvas_helper.create_camera();
@@ -298,7 +467,14 @@ function init() {
   // Define the renderer
   renderer = new THREE.WebGLRenderer();
   renderer.setPixelRatio( window.devicePixelRatio );
-  canvas_helper.set_canvas_holder_size(canvas_holder, renderer);
+
+  labelRenderer = new CSS2DRenderer();
+  // labelRenderer.setSize( window.innerWidth, window.innerHeight );
+  labelRenderer.domElement.style.position = 'absolute';
+  // labelRenderer.domElement.style.top = '0px';
+  canvas_holder.appendChild( labelRenderer.domElement );
+
+  canvas_helper.set_canvas_holder_size(canvas_holder, renderer, labelRenderer);
   canvas_holder.appendChild( renderer.domElement );
   
   // Define the scene
@@ -310,8 +486,7 @@ function init() {
   light.position.set( -150, -1050, 800).normalize();
   scene.add( light );
 
-  // Adds the geometry
-  initGeometry();
+  initGeometry();  // Adds the geometry
 
   // Add event listeners
 	document.addEventListener( 'mousedown', onDocumentMouseDown, false );
@@ -320,10 +495,8 @@ function init() {
   render();
 }
 
-
 // Event listener for mouse clicks
 function onDocumentMouseDown( event ) {
-  // Finds the id of the city clicked
   function findIdCityClicked(raycaster) {
     // Find intersections with inner pucks
     const intersects = raycaster.intersectObjects( v_city_puck_inner, true );
@@ -351,34 +524,21 @@ function onDocumentMouseDown( event ) {
 
   var obj_clicked = v_city_puck_inner[id_city_clicked];
 
-  // First city gets raised
-  if (vStartEndCity.length == 0) {
-    adjustRaise(obj_clicked);
-    obj_clicked.material.color.set(rgb_path);
-  
-  // City in the list gets lowered
-  } else if (vStartEndCity.length == 1) {
-    const id_lower = vStartEndCity[0];
-    const object_lower = scene.getObjectById(id_lower);
-    adjustLower(object_lower);
-    object_lower.material.color.set(rgb_puck_inner);
-  }
-
   const id_raise = obj_clicked.id;
-  manageStartEndCity(id_raise);
   
+  markers.updateMarkers(id_raise);
   drawPath();
-
   render();
 }
 
+
 // Draws the path between the two clicked cities
 function drawPath() {
-  if (vStartEndCity.length != 2) { return; }
+  if (markers.vStartEndCity.length != 2) { return; }
 
   var v_id_path = helper_cpp.loadPathReal( 
-    dictObIdToCityId[vStartEndCity[0]],
-    dictObIdToCityId[vStartEndCity[1]],
+    dictObIdToCityId[markers.vStartEndCity[0]],
+    dictObIdToCityId[markers.vStartEndCity[1]],
   );
 
   // Log to console
@@ -405,54 +565,44 @@ function drawPath() {
     v_cylinders.push(cylinder);
   }
 
-  // Move the remaining cylinders off the screen
-  for (let i = n_cyl_required; i < v_cylinders.length; i++) {
+  // Move all cylinders off the screen
+  for (let i = 0; i < v_cylinders.length; i++) {
     const cylinder = v_cylinders[i];
     cylinder.position.set(0, 0, -1000);
   }
-  
+
+  // id city last clicked
+  const id_last = markers.getCityIdLast();
+
   // Adds a cylinder between each pair of points
   for (let i = 1; i < points.length; i++) {
     const cylinder = v_cylinders[i - 1];
     const xyz_0 = points[i - 1];
     const xyz_1 = points[i];
-    adjustCylinder(cylinder, xyz_0, xyz_1);
-  }
-}
 
-function manageStartEndCity(id) {
-  if (vStartEndCity.length <= 1) {
-    vStartEndCity.push(id);
-    return -1;
-  } else {
-    // remove the first city from the list 
-    const id_remove = vStartEndCity.shift();
-    vStartEndCity.push(id);
-    return id_remove;
+    setTimeout(() => {
+      if (id_last != markers.getCityIdLast()) { return; }
+      adjustCylinder(cylinder, xyz_0, xyz_1);
+      render();
+    }, i * 70);
   }
 }
 
 function onWindowResize() {
   console.log("CW" + canvas_holder.clientWidth + " CH" + canvas_holder.clientHeight);
-  canvas_helper.set_canvas_holder_size(canvas_holder, renderer);
+  canvas_helper.set_canvas_holder_size(canvas_holder, renderer, labelRenderer);
   render();
 }
-
-// function animate() {
-//   requestAnimationFrame(animate);
-//   render();
-// }
 
 function render() {
   camera.position.set(-168, -1000, 800)
   camera.lookAt( 0, 0, 0 );
+  camera.rotation.setFromRotationMatrix(camera_vectors.uv_mat);
+
+  camera.updateMatrixWorld(); 
   
-  // tilt camera counter clockwise, look from above
-  camera.rotation.z = THREE.MathUtils.degToRad( -11 );
-
-  camera.updateMatrixWorld();
-
   renderer.render( scene, camera );
+  labelRenderer.render( scene, camera );
 
   console.log("render");
 }
